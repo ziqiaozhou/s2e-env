7363ac7dfc8e7e8d480d0a52017fde74e18bfeaa
Adrian Herrera <adrian.herrera02@gmail.com>
2018-09-29 16:38:36 +1000

    project_creation: refactored project configuration
    Removed the configuration class with a project class hierarchy instead. This
    makes it easier to create custom projects programatically, without having to
    also write custom configuration classes.

--- s2e_env/commands/new_project.py
+++ s2e_env/commands/new_project.py
@@ -29,10 +29,9 @@ import re
 from magic import Magic
 
 from s2e_env.command import EnvCommand, CommandError
-from s2e_env.commands.project_creation import Project
-from s2e_env.commands.project_creation.config import \
-    CGCProjectConfiguration, LinuxProjectConfiguration, WindowsProjectConfiguration, WindowsDLLProjectConfiguration, \
-    WindowsDriverProjectConfiguration
+from s2e_env.commands.project_creation.cgc_project import CGCProject
+from s2e_env.commands.project_creation.linux_project import LinuxProject
+from s2e_env.commands.project_creation.windows_project import WindowsProject, WindowsDLLProject, WindowsDriverProject
 from s2e_env.infparser.driver import Driver
 from s2e_env.manage import call_command
 
@@ -53,10 +52,10 @@ MSDOS_REGEX = re.compile(r'^MS-DOS executable')
 DLL32_REGEX = re.compile(r'^PE32 executable \(DLL\)')
 DLL64_REGEX = re.compile(r'^PE32\+ executable \(DLL\)')
 
-PROJECT_CONFIGS = {
-    'cgc': CGCProjectConfiguration,
-    'linux': LinuxProjectConfiguration,
-    'windows': WindowsProjectConfiguration
+PROJECT_TYPES = {
+    'cgc': CGCProject,
+    'linux': LinuxProject,
+    'windows': WindowsProject,
 }
 
 
@@ -92,30 +91,29 @@ def _get_arch(target_path):
     i386, x64, etc.) is also checked.
 
     Returns:
-        A tuple containing the target's architecture and a project
-        configuration class is returned.
+        A tuple containing the target's architecture and a project class is
+        returned.
     """
     default_magic = Magic()
     magic_checks = [
-        (Magic(magic_file=CGC_MAGIC), CGC_REGEX, CGCProjectConfiguration, 'i386'),
-        (default_magic, ELF32_REGEX, LinuxProjectConfiguration, 'i386'),
-        (default_magic, ELF64_REGEX, LinuxProjectConfiguration, 'x86_64'),
-        (default_magic, DLL32_REGEX, WindowsDLLProjectConfiguration, 'i386'),
-        (default_magic, DLL64_REGEX, WindowsDLLProjectConfiguration, 'x86_64'),
-        (default_magic, PE32_REGEX, WindowsProjectConfiguration, 'i386'),
-        (default_magic, PE64_REGEX, WindowsProjectConfiguration, 'x86_64'),
-        (default_magic, MSDOS_REGEX, WindowsProjectConfiguration, 'i386')
+        (Magic(magic_file=CGC_MAGIC), CGC_REGEX, CGCProject, 'i386'),
+        (default_magic, ELF32_REGEX, LinuxProject, 'i386'),
+        (default_magic, ELF64_REGEX, LinuxProject, 'x86_64'),
+        (default_magic, DLL32_REGEX, WindowsDLLProject, 'i386'),
+        (default_magic, DLL64_REGEX, WindowsDLLProject, 'x86_64'),
+        (default_magic, PE32_REGEX, WindowsProject, 'i386'),
+        (default_magic, PE64_REGEX, WindowsProject, 'x86_64'),
+        (default_magic, MSDOS_REGEX, WindowsProject, 'i386')
     ]
 
     # Check the target program against the valid file types
-    for magic_check, regex, proj_config_class, arch in magic_checks:
+    for magic_check, regex, proj_class, arch in magic_checks:
         magic = magic_check.from_file(target_path)
         matches = regex.match(magic)
 
-        # If we find a match, create that project. The user instructions
-        # are returned
+        # If we find a match, create that project
         if matches:
-            return arch, proj_config_class
+            return arch, proj_class
 
     return None, None
 
@@ -146,8 +144,7 @@ def _gen_win_driver_project(target_path, file_paths, *args, **options):
     options['modules'] = [(os.path.basename(first_sys_file), True)]
     options['processes'] = []
 
-    cfg = WindowsDriverProjectConfiguration()
-    call_command(Project(cfg), *args, **options)
+    call_command(WindowsDriverProject(), *args, **options)
 
 
 def _handle_inf(target_path, *args, **options):
@@ -183,7 +180,7 @@ def _handle_sys(target_path, *args, **options):
 
 
 def _handle_generic_target(target_path, *args, **options):
-    arch, proj_config_class = _get_arch(target_path)
+    arch, proj_class = _get_arch(target_path)
     if not arch:
         raise CommandError('%s is not a valid target for S2E analysis' % target_path)
 
@@ -196,11 +193,10 @@ def _handle_generic_target(target_path, *args, **options):
     options['modules'] = [(os.path.basename(target_path), False)]
 
     options['processes'] = []
-    if not isinstance(proj_config_class, WindowsDLLProjectConfiguration):
+    if not isinstance(proj_class, WindowsDLLProject):
         options['processes'].append(os.path.basename(target_path))
 
-    cfg = proj_config_class()
-    call_command(Project(cfg), *args, **options)
+    call_command(proj_class(), *args, **options)
 
 
 def _handle_with_file(target_path, *args, **options):
@@ -233,10 +229,10 @@ def _handle_empty_project(*args, **options):
         raise CommandError('An empty project requires a name. Use the -n '
                            'option to specify one')
 
-    configs = PROJECT_CONFIGS.keys()
-    if options['type'] not in configs:
+    project_types = PROJECT_TYPES.keys()
+    if options['type'] not in project_types:
         raise CommandError('An empty project requires a type. Use the -t '
-                           'option and specify one from %s' % configs)
+                           'option and specify one from %s' % project_types)
 
     options['target_files'] = []
     options['target_arch'] = None
@@ -247,8 +243,8 @@ def _handle_empty_project(*args, **options):
     options['modules'] = []
     options['processes'] = []
 
-    cfg = PROJECT_CONFIGS[options['type']]()
-    call_command(Project(cfg), *args, **options)
+    project = PROJECT_TYPES[options['type']]
+    call_command(project(), *args, **options)
 
 
 class Command(EnvCommand):
@@ -288,7 +284,7 @@ class Command(EnvCommand):
 
         parser.add_argument('-t', '--type', required=False, default=None,
                             help='Project type (%s), valid only when creating empty projects' %
-                            ','.join(PROJECT_CONFIGS.keys()))
+                            ','.join(PROJECT_TYPES.keys()))
 
         parser.add_argument('-s', '--use-seeds', action='store_true',
                             help='Use this option to use seeds for creating '
